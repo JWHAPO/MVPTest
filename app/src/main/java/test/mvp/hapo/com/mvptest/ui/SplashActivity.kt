c3d7094b60e63d2e1a0449518ac5023e7112fa14
JWHAPO <kim2101714@gmail.com>
2019-01-23 12:54:13 +0900

    add kotlin extension

--- app/src/main/java/test/mvp/hapo/com/mvptest/ui/SplashActivity.kt
+++ app/src/main/java/test/mvp/hapo/com/mvptest/ui/SplashActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.splash_layout.*
 import test.mvp.hapo.com.mvptest.R
 import test.mvp.hapo.com.mvptest.ui.main.MainActivity
 
@@ -24,15 +24,12 @@ class SplashActivity : AppCompatActivity(), SplashContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_layout)
 
-        val button01 = findViewById<Button>(R.id.button01)
-        val button02 = findViewById<Button>(R.id.button02)
-        val button03 = findViewById<Button>(R.id.button03)
         button01.setOnClickListener(clickListener)
         button02.setOnClickListener(clickListener)
         button03.setOnClickListener(clickListener)
     }
 
-    val clickListener = View.OnClickListener { view->
+    private val clickListener = View.OnClickListener { view->
         when(view.id){
             R.id.button01 -> showToast("1")
             R.id.button02 -> showToast("2")
@@ -40,7 +37,7 @@ class SplashActivity : AppCompatActivity(), SplashContract.View {
         }
     }
 
-    fun goMainActivity(){
+    private fun goMainActivity(){
         val intent = Intent(this@SplashActivity, MainActivity::class.java)
         val message = "go Main!"
 
